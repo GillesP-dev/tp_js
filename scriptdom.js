d2b3909ad934efc18d2d45e25129d9ea63202a6b
GillesP-dev <117928509+GillesP-dev@users.noreply.github.com>
2023-01-13 14:07:26 +0100

    exercices js nouveau

---
+++ scriptdom.js
@@ -0,0 +1,71 @@
+
+let btn = document.getElementById('btn');
+
+
+/*btn.addEventListener('click',event  => {
+const para = document.createElement('h2');
+para.innerText = "Hello World";
+document.body.appendChild(para);
+});*/
+/*
+let rech = document.getElementById('recherche').value;
+
+btn.addEventListener('click',event => {
+const para = document.createElement('p');
+para.innerText = rech;
+document.body.appendChild(para);
+})*/
+
+//----------------------------------------------------------------
+
+// let para = document.querySelectorAll('h2');
+// console.log(para);
+// para.forEach(element => {
+// element.addEventListener('click', () => {
+//     console.log(element)
+//     if(element.nextElementSibling.style.display != "none" ){
+//         element.nextElementSibling.style.display='none';
+        
+//     }else {
+//         element.nextElementSibling.style.display='block';
+        
+//     }
+//     });
+// })
+
+//---------------------------------------------------------------
+// let message = prompt("voulais vous un paragraphe");
+// window.addEventListener('load', (event) => {
+    
+//     if(message == "oui"){
+//         const para = document.createElement('p');
+//         para.textContent = "Voici un nouveau paragraphe";
+//         document.body.appendChild(para);
+//     }
+//   });
+
+ //---------------------------------------------------------------
+ 
+ function createDom(element_name, text, parent) {
+    const markup = document.createElement(element_name);
+    markup.textContent = text;
+    parent.appendChild(markup);
+    return markup;
+}
+
+createDom('button','Tous',document.body).setAttribute('id','btn');
+createDom('button','HTML',document.body).setAttribute('id','btn');
+createDom('button','CSS',document.body).setAttribute('id','btn');
+createDom('button','JS',document.body).setAttribute('id','btn');
+
+const btnHtml = document.getElementById('btn2');
+const btnCss = document.getElementById('btn3');
+const btnJs = document.getElementById('btn4');
+const btnTous = document.getElementById('btn1');
+
+const bouton = document.querySelectorAll('button');
+console.log(bouton)
+
+//array.forEach(element => {
+    
+//});
